e75b7cc79dd031093cbae400bf79cb641ea04db6
Barla Ankit <barla.x88@gmail.com>
2022-09-21 11:49:16 +0530

    Setup listners for homepage and marketplace

--- public/index.php
+++ public/index.php
@@ -3,10 +3,12 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use app\controllers\UserController;
+use app\controllers\MainController;
 use app\Router;
 
 $router = new Router();
 
+// User Pages
 $router->get('/user', [UserController::class, 'redirect']);
 
 //actually vaidate user
@@ -20,5 +22,10 @@ $router->get('/user/profile', [UserController::class, 'login']);
 $router->get('/user/logout', [UserController::class, 'logout']);
 $router->post('/user/register', [UserController::class, 'register']);
 $router->get('/user/register', [UserController::class, 'register']);
+
+
+//Index pages
+$router->get('/', [MainController::class, 'index']);
+$router->get('/main/marketplace', [MainController::class, 'marketplace']);
 
 $router->resolve();
